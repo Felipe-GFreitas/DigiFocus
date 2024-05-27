5a5eb83cf15265e5f87b31e4e4b7f0ce3be1b913
Felipe Freitas <flp266.fg@gmail.com>
2024-05-27 18:22:50 -0300

    Release 0.002
    Adição de nova tela, e configuração basicas.

---
+++ app/src/main/java/com/example/digifocus/ConfigurationsActivity.kt
@@ -0,0 +1,36 @@
+package com.example.digifocus
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfigurationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val spinnerTimeOptions: Spinner = findViewById(R.id.spinnerTimeOptions)
+        val spinnerImageOptions: Spinner = findViewById(R.id.spinnerImageOptions)
+        val applyButton: Button = findViewById(R.id.applyButton)
+
+        val timeOptions = arrayOf("10:00", "15:00", "20:00", "25:00")
+        val timeAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, timeOptions)
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerTimeOptions.adapter = timeAdapter
+
+        val imageOptions = arrayOf("egg1", "egg2", "egg3", "egg4")
+        val imageAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, imageOptions)
+        imageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerImageOptions.adapter = imageAdapter
+
+        applyButton.setOnClickListener {
+
+            val selectedTime = spinnerTimeOptions.selectedItem.toString()
+            val selectedImage = spinnerImageOptions.selectedItem.toString()
+
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/digifocus/MainActivity.kt
+++ app/src/main/java/com/example/digifocus/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.digifocus
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,12 +11,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textViewTimer: TextView
     private lateinit var buttonStartPause: Button
     private lateinit var countDownTimer: CountDownTimer
-    private lateinit var spinnerTimeOptions: Spinner
-    private lateinit var spinnerImageOptions: Spinner
     private lateinit var imageView: ImageView
+    private lateinit var progressBarXP: ProgressBar
+    private lateinit var settingsButton: Button
 
     private var isTimerRunning = false
-    private var timeLeftInMillis: Long = 1500000
+    private var timeLeftInMillis: Long = 1500000 // 25 minutes in milliseconds
     private var startTimeInMillis: Long = 1500000
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,24 +25,9 @@ class MainActivity : AppCompatActivity() {
 
         textViewTimer = findViewById(R.id.textViewTimer)
         buttonStartPause = findViewById(R.id.buttonStartPause)
-        spinnerTimeOptions = findViewById(R.id.spinnerTimeOptions)
-        spinnerImageOptions = findViewById(R.id.spinnerImageOptions)
         imageView = findViewById(R.id.imageView)
-
-        val timeOptions = arrayOf("10:00", "15:00", "20:00", "25:00")
-        val timeAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, timeOptions)
-        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerTimeOptions.adapter = timeAdapter
-
-        spinnerTimeOptions.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedTime = timeOptions[position]
-                updateTime(selectedTime)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-        }
+        progressBarXP = findViewById(R.id.progressBarXP)
+        settingsButton = findViewById(R.id.settingsButton)
 
         buttonStartPause.setOnClickListener {
             if (isTimerRunning) {
@@ -52,36 +37,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        updateCountDownText()
-
-        val imageOptions = arrayOf("patamon", "digimoncorno", "greymon", "pumpmon")
-        val imageAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, imageOptions)
-        imageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerImageOptions.adapter = imageAdapter
-
-        spinnerImageOptions.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedImage = imageOptions[position]
-                updateImage(selectedImage)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, ConfigurationsActivity::class.java)
+            startActivity(intent)
         }
-    }
 
-    private fun updateTime(selectedTime: String) {
-        val timeInMinutes = selectedTime.substringBefore(":").toInt()
-        val timeInMillis = timeInMinutes * 60000L
-
-        timeLeftInMillis = timeInMillis
-        startTimeInMillis = timeInMillis
         updateCountDownText()
-    }
-
-    private fun updateImage(selectedImage: String) {
-        val resId = resources.getIdentifier(selectedImage, "drawable", packageName)
-        imageView.setImageResource(resId)
     }
 
     private fun startTimer() {
@@ -89,11 +50,13 @@ class MainActivity : AppCompatActivity() {
             override fun onTick(millisUntilFinished: Long) {
                 timeLeftInMillis = millisUntilFinished
                 updateCountDownText()
+                updateProgressBar()
             }
 
             override fun onFinish() {
                 isTimerRunning = false
                 buttonStartPause.text = "Start"
+                progressBarXP.progress = 100 // Set progress to full when timer finishes
             }
         }.start()
 
@@ -113,5 +76,10 @@ class MainActivity : AppCompatActivity() {
 
         val timeFormatted = String.format("%02d:%02d", minutes, seconds)
         textViewTimer.text = timeFormatted
+    }
+
+    private fun updateProgressBar() {
+        val progress = ((startTimeInMillis - timeLeftInMillis).toFloat() / startTimeInMillis * 100).toInt()
+        progressBarXP.progress = progress
     }
 }
